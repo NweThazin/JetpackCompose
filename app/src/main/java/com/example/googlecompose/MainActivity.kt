ec26b61d6556c7b2ad7d156b4c2245e7f6a7c3e9
Nwe Thazin <nwe.nwethazin@gmail.com>
2022-10-05 15:39:52 +0800

    - change to rememberSaveable to persist state when configuration change

--- app/src/main/java/com/example/googlecompose/MainActivity.kt
+++ app/src/main/java/com/example/googlecompose/MainActivity.kt
@@ -17,6 +17,7 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -37,7 +38,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MyApp() {
-    var shouldShowOnBoarding by remember { mutableStateOf(true) }
+    var shouldShowOnBoarding by rememberSaveable { mutableStateOf(true) }
     if (shouldShowOnBoarding) {
         OnBoardingScreen(onContinueClicked = {
             shouldShowOnBoarding = false
