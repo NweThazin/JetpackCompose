7aef551a290b1897579653a8c7ff1b92940d1f33
Nwe Thazin <nwe.nwethazin@gmail.com>
2022-10-05 14:18:56 +0800

    - state in compose

--- app/src/main/java/com/example/googlecompose/MainActivity.kt
+++ app/src/main/java/com/example/googlecompose/MainActivity.kt
@@ -11,6 +11,8 @@ import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -44,6 +46,7 @@ fun MyApp(names: List<String> = listOf("World", "Compose")) {
 
 @Composable
 fun Greeting(name: String) {
+    val expanded = remember { mutableStateOf(false) }
     Surface(
         color = MaterialTheme.colors.primary,
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
@@ -53,8 +56,10 @@ fun Greeting(name: String) {
                 Text(text = "Hello,")
                 Text(text = name)
             }
-            OutlinedButton(onClick = { /*TODO*/ }) {
-                Text(text = "Show More")
+            OutlinedButton(onClick = {
+                expanded.value = !expanded.value
+            }) {
+                Text(text = if (expanded.value) "Show less" else "Show more")
             }
         }
     }
