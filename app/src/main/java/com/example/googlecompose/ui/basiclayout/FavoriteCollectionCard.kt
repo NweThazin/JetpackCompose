31ed0cb517d5a6352ed3997a8ea1edf79d129293
Nwe Thazin <nwe.nwethazin@gmail.com>
2022-10-06 21:29:46 +0800

    Basic layouts in Compose
    - Favorite collection card - material surface

--- app/src/main/java/com/example/googlecompose/ui/basiclayout/FavoriteCollectionCard.kt
+++ app/src/main/java/com/example/googlecompose/ui/basiclayout/FavoriteCollectionCard.kt
@@ -3,17 +3,16 @@ package com.example.googlecompose.ui.basiclayout
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.paddingFromBaseline
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -22,38 +21,38 @@ import androidx.compose.ui.unit.dp
 import com.example.googlecompose.R
 
 @Composable
-fun AlignYourBodyElement(
+fun FavoriteCollectionCard(
     @StringRes title: Int,
     @DrawableRes drawable: Int,
     modifier: Modifier = Modifier
 ) {
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
+    Surface(
+        shape = MaterialTheme.shapes.small,
         modifier = modifier
     ) {
-        Image(
-            painter = painterResource(id = drawable),
-            contentDescription = null,
-            contentScale = ContentScale.Crop,
-            modifier = Modifier
-                .size(88.dp)
-                .clip(CircleShape)
-        )
-        Text(
-            text = stringResource(id = title),
-            style = MaterialTheme.typography.body2,
-            modifier = Modifier.paddingFromBaseline(
-                top = 24.dp,
-                bottom = 8.dp
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.width(192.dp)
+        ) {
+            Image(
+                painter = painterResource(id = drawable),
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.size(56.dp) // image height
             )
-        )
+            Text(
+                text = stringResource(id = title),
+                style = MaterialTheme.typography.body2, // use body for text size
+                modifier = Modifier.padding(horizontal = 16.dp) // left and right padding
+            )
+        }
     }
 }
 
-@Preview(showBackground = true, backgroundColor = 0xFFF0EAE2)
+@Preview
 @Composable
-fun PreviewAlignYourBodyElement() {
-    AlignYourBodyElement(
+fun PreviewFavoriteCollectionCard() {
+    FavoriteCollectionCard(
         R.string.label_working_hard,
         R.drawable.qoobee1,
         Modifier.padding(8.dp)
